2523ac7ab433d1d70dd0e94d176732aa64088444
陳 <58775004+Amber-1995@users.noreply.github.com>
2023-01-01 20:38:56 +0900

    Create Singleton.h

---
+++ ExtraX/Core/ExtraX/src/ExtraX/Singleton.h
@@ -0,0 +1,43 @@
+#pragma once
+#ifndef EXTRAX_DESIGN_PATTERN_H
+#define EXTRAX_DESIGN_PATTERN_H
+
+namespace ExtraX
+{
+
+	template<typename INSTANCE_TYPE>
+	class Singleton
+	{
+	private:
+		inline static std::unique_ptr<INSTANCE_TYPE> _instance = nullptr;
+
+		inline static INSTANCE_TYPE* CreateSingleton()
+		{
+			return new INSTANCE_TYPE();
+		}
+
+
+	public:
+		Singleton() = delete;
+
+		inline static INSTANCE_TYPE* GetSingleton()
+		{
+			if (_instance.get())
+			{
+				return _instance.get();
+			}
+
+			_instance.reset(CreateSingleton());
+			return _instance.get();
+		}
+	};
+
+	template<typename INSTANCE_TYPE>
+	INSTANCE_TYPE* (*GetSingleton)() = ::ExtraX::Singleton<INSTANCE_TYPE>::GetSingleton;
+}
+
+
+
+#endif // !EXTRAX_DESIGN_PATTERN_H
+
+
